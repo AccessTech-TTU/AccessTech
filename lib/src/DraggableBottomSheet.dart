f74cd51fbe2de51a88b3811bffd22139f079d310
rajnraman <rajnraman@gmail.com>
2023-11-11 17:04:11 -0600

    bottom sheet changes

--- lib/src/DraggableBottomSheet.dart
+++ lib/src/DraggableBottomSheet.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'buildingData.dart';
+import 'building_screen.dart';
 
 class MyDraggableSheet extends StatefulWidget {
-  const MyDraggableSheet({super.key});
+  const MyDraggableSheet({Key? key});
+
   @override
   _MyDraggableSheetState createState() => _MyDraggableSheetState();
 }
@@ -10,42 +12,29 @@ class MyDraggableSheet extends StatefulWidget {
 class _MyDraggableSheetState extends State<MyDraggableSheet> {
   final _sheet = GlobalKey();
   final _controller = DraggableScrollableController();
+  String searchQuery = '';
 
-  @override
-  void initState() {
-    super.initState();
-    _controller.addListener(_onChanged);
+  void _removeFromFavorites(BuildingInfo building) {
+    setState(() {
+      favoriteBuildings.remove(building);
+    });
   }
 
-  void _onChanged() {
-    final currentSize = _controller.size;
-    if (currentSize <= 0.05) _collapse();
+  void _updateFavorites() {
+    setState(() {
+      // This empty function body is used to trigger a rebuild
+    });
   }
 
-  void _collapse() => _animateSheet(sheet.snapSizes!.first);
-
-  void _anchor() => _animateSheet(sheet.snapSizes!.last);
-
-  void _expand() => _animateSheet(sheet.maxChildSize);
-
-  void _hide() => _animateSheet(sheet.minChildSize);
-
-  void _animateSheet(double size) {
-    _controller.animateTo(
-      size,
-      duration: const Duration(milliseconds: 50),
-      curve: Curves.easeInOut,
-    );
+  void _addToFavorites(BuildingInfo building) {
+    // Check if the building is not already in the favorites list
+    if (!favoriteBuildings.contains(building)) {
+      // Add the building to the favorites list
+      favoriteBuildings.add(building);
+      // Notify the UI that the list has changed
+      _updateFavorites();
+    }
   }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _controller.dispose();
-  }
-
-  DraggableScrollableSheet get sheet =>
-      (_sheet.currentWidget as DraggableScrollableSheet);
 
   @override
   Widget build(BuildContext context) {
@@ -55,10 +44,10 @@ class _MyDraggableSheetState extends State<MyDraggableSheet> {
           child: DraggableScrollableSheet(
             key: _sheet,
             initialChildSize: 0.5,
-            maxChildSize: 1,
-            minChildSize: 0.15,
+            maxChildSize: .94,
+            minChildSize: 0.2,
             snap: true,
-            snapSizes: [0.15, 0.5, 1],
+            snapSizes: [0.2, 0.5, .94],
             controller: _controller,
             builder: (BuildContext context, ScrollController scrollController) {
               return Container(
@@ -83,23 +72,33 @@ class _MyDraggableSheetState extends State<MyDraggableSheet> {
                         ),
                       ],
                     ),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Search',
-                        prefixIcon: Icon(Icons.search),
-                      ),
-                      onTap: () {
+                    GestureDetector(
+                      onVerticalDragDown: (_) {
                         _expand();
                       },
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Search',
+                          prefixIcon: Icon(Icons.search),
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            searchQuery = value.toLowerCase();
+                          });
+                        },
+                      ),
                     ),
                     Padding(
-                        padding: EdgeInsets.all(8),
-                        child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                                style: TextStyle(fontSize: 18), "Favorites"))),
+                      padding: EdgeInsets.all(8),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Favorites",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ),
                     ListView.builder(
-                      //controller: scrollController,
                       shrinkWrap: true,
                       physics: NeverScrollableScrollPhysics(),
                       itemCount: favoriteBuildings.length,
@@ -110,32 +109,30 @@ class _MyDraggableSheetState extends State<MyDraggableSheet> {
                           child: ListTile(
                             leading: Icon(Icons.star),
                             title: Text(building.name),
-                            onTap: () {},
+                            onTap: () {
+                              _showBuildingDetails(context, building);
+                            },
                           ),
                         );
                       },
                     ),
                     Padding(
-                        padding: EdgeInsets.all(8),
-                        child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                                style: TextStyle(fontSize: 18), "Buildings"))),
+                      padding: EdgeInsets.all(8),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Buildings",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ),
                     ListView.builder(
-                      //controller: scrollController,
                       shrinkWrap: true,
                       physics: NeverScrollableScrollPhysics(),
                       itemCount: buildingData.length,
                       itemBuilder: (context, index) {
                         final building = buildingData[index];
-                        return Card(
-                          elevation: 1,
-                          child: ListTile(
-                            leading: Icon(Icons.house),
-                            title: Text(building.name),
-                            onTap: () {},
-                          ),
-                        );
+                        return _buildBuildingCard(building);
                       },
                     ),
                   ],
@@ -143,6 +140,174 @@ class _MyDraggableSheetState extends State<MyDraggableSheet> {
               );
             },
           ),
+        );
+      },
+    );
+  }
+
+
+  Widget _buildBuildingCard(BuildingInfo building) {
+    final isFavorite = favoriteBuildings.contains(building);
+
+    // Check if the building name contains the search query
+    if (searchQuery.isEmpty ||
+        building.name.toLowerCase().contains(searchQuery)) {
+      return Card(
+        elevation: 1,
+        child: ListTile(
+          leading: Icon(Icons.house),
+          title: Text(building.name),
+          onTap: () {
+            _showBuildingDetails(context, building);
+          },
+        ),
+      );
+    } else {
+      return Container(); // Return an empty container for non-matching items
+    }
+  }
+
+  void _onChanged() {
+    final currentSize = _controller.size;
+    if (currentSize <= 0.05) _collapse();
+  }
+
+  void _collapse() => _animateSheet(sheet.snapSizes!.first);
+
+  void _anchor() => _animateSheet(sheet.snapSizes!.last);
+
+  void _expand() => _animateSheet(sheet.maxChildSize);
+
+  void _hide() => _animateSheet(sheet.minChildSize);
+
+  void _animateSheet(double size) {
+    _controller.animateTo(
+      size,
+      duration: const Duration(milliseconds: 50),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  DraggableScrollableSheet get sheet =>
+      (_sheet.currentWidget as DraggableScrollableSheet);
+
+  void _showBuildingDetails(BuildContext context, BuildingInfo building) {
+    final bool isFavorite = favoriteBuildings.contains(building);
+
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return DraggableScrollableSheet(
+          initialChildSize: 1,
+          minChildSize: 0.15,
+          maxChildSize: 1,
+          builder: (BuildContext context, ScrollController scrollController) {
+            return Padding(
+              padding: const EdgeInsets.all(16),
+              child: SingleChildScrollView(
+                controller: scrollController,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.house), // Icon for building
+                        SizedBox(width: 8),
+                        Expanded(
+                          child: FittedBox(
+                            fit: BoxFit.scaleDown,
+                            child: Text(
+                              building.name,
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    Text('${building.description}'),
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        isFavorite
+                            ? _removeFromFavorites(building)
+                            : _addToFavorites(building);
+                      },
+                      icon: Icon(
+                        Icons.star,
+                        color: isFavorite ? Colors.white : null,
+                      ),
+                      label: Text(
+                        isFavorite
+                            ? 'Remove from Favorites'
+                            : 'Add to Favorites',
+                      ),
+                    ),
+                    Text(
+                      'Hours:',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text('${building.hours}'),
+                    Text(
+                      'Accessible:',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    ListTile(
+                      title: Row(
+                        children: [
+                          Icon(Icons.door_sliding), // Icon for doors
+                          SizedBox(width: 8),
+                          Text('Doors'),
+                        ],
+                      ),
+                    ),
+                    ListTile(
+                      title: Row(
+                        children: [
+                          Icon(Icons.wheelchair_pickup_rounded),
+                          // Icon for ramps
+                          SizedBox(width: 8),
+                          Text('Ramps'),
+                        ],
+                      ),
+                    ),
+                    ListTile(
+                      title: Row(
+                        children: [
+                          Icon(Icons.elevator), // Icon for elevator
+                          SizedBox(width: 8),
+                          Text('Elevator'),
+                        ],
+                      ),
+                    ),
+                    ListTile(
+                      title: Row(
+                        children: [
+                          Icon(Icons.wc), // Icon for restroom
+                          SizedBox(width: 8),
+                          Text('Restroom'),
+                        ],
+                      ),
+                    ),
+                    Text(
+                      'Address:',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text('${building.address}'),
+                    SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Implement your report issue logic here
+                      },
+                      child: Text('Report Issue'),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
         );
       },
     );
--- lib/src/building_screen.dart
+++ lib/src/building_screen.dart
@@ -1,23 +1,82 @@
-import 'package:flutter/material.dart';
-import "buildingData.dart";
+// import 'package:flutter/material.dart';
+// import "buildingData.dart";
+//
+// /*
+// Authors:
+//   Thinh Pham, Travis Libre
+// Description:
+//   This file is the buildings screen. It currently lists the buildings,
+//   and a description of them. Currently, much of it is unused and must
+//   be implemented.
+// */
+//
+// class BuildingInfo {
+//   final String name;
+//   final String description;
+//   final String hours;
+//   final String address;
+//   final String imageUrl;
+//
+//   BuildingInfo({
+//     required this.name,
+//     required this.description,
+//     required this.hours;
+//     required this.address;
+//     required this.imageUrl,
+//   });
+// }
+//
+// class BuildingCard extends StatelessWidget {
+//   final BuildingInfo building;
+//
+//   const BuildingCard({Key? key, required this.building}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Card(
+//       elevation: 10,
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           Padding(
+//             padding: const EdgeInsets.all(8),
+//             child: Text(
+//               building.name,
+//               style: TextStyle(
+//                 fontSize: 20,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
+//             child: Text(
+//               building.description,
+//               style: TextStyle(fontSize: 16),
+//             ),
+//           ),
+//         ], // Children (list)
+//       ),
+//     );
+//   }
+// }
 
-/*
-Authors:
-  Thinh Pham, Travis Libre
-Description:
-  This file is the buildings screen. It currently lists the buildings,
-  and a description of them. Currently, much of it is unused and must
-  be implemented.
-*/
+
+import 'package:flutter/material.dart';
+import 'buildingData.dart';
 
 class BuildingInfo {
   final String name;
   final String description;
+  final String hours;
+  final String address;
   final String imageUrl;
 
   BuildingInfo({
     required this.name,
     required this.description,
+    required this.hours,
+    required this.address,
     required this.imageUrl,
   });
 }
@@ -47,16 +106,29 @@ class BuildingCard extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
             child: Text(
-              building.description,
+              'Description: ${building.description}',
               style: TextStyle(fontSize: 16),
             ),
           ),
-        ], // Children (list)
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
+            child: Text(
+              'Hours: ${building.hours}',
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
+            child: Text(
+              'Address: ${building.address}',
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-
 // Start of BuildingScreenUI
 class BuildingScreen extends StatelessWidget {
   const BuildingScreen({Key? key}) : super(key: key);
